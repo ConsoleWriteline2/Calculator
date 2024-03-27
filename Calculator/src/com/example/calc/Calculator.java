7e6153eccf0b1c186e6a9f3b2c975c79adf1b65d
George P <George_P@example.com>
2024-03-27 13:54:34 +0300

    GUI

---
+++ Calculator/src/com/example/calc/Calculator.java
@@ -0,0 +1,42 @@
+package com.example.calc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Calculator extends Activity implements OnClickListener, TextWatcher {
+	EditText etResult;
+	Button btnPlus, btnMinus, btnMultiply, btnDivide, btnClear, btnAbs, btnDiv, btnMod, btnStepen, button0, button1,
+			button2, button3, button4, button5, button6, button7, button8, button9, btnCalculate;
+
+	int int_num1, int_num2, int_result;
+	double double_num1, double_num2, double_result;
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void afterTextChanged(Editable s) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
